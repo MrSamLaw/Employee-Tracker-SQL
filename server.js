43ff26e15be88bd9fcd365fcc4bd0cee2399781e
Mr Sam Law <sam@chongland.com>
2021-05-20 17:47:35 +1000

    connection & ASCII Logo

--- server.js
+++ server.js
@@ -1,2 +1,29 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const logo = require('asciiart-logo');
+const connection = require('./config/connection');
+
+connection.connect((err) => {
+    if (err) throw err;
+    initScreen();
+});
+
+initScreen = () => {
+    console.log(
+        logo({
+            name: 'EMPLOYEE MANAGER',
+            font: '3D-ASCII',
+            lineChars: 10,
+            padding: 3,
+            margin: 1,
+            borderColor: 'white',
+            logoColor: 'bold-white',
+            textColor: 'white',
+        })
+            .emptyLine()
+            .right('version 1.0.0')
+            .emptyLine()
+            .center('Simple management of your Employees')
+            .render()
+    );
+}
